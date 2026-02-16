ce57ee2d10e1ebdd26ff1bc46e2cdb85135e8f74
Omar-MK-20 <omar.mk.201@gmail.com>
2026-02-16 18:55:38 +0200

    feat: add SECRET_KEY export to app configuration

--- config/app.config.js
+++ config/app.config.js
@@ -6,3 +6,4 @@ dotenv.config({ path: path.resolve("./config/.env.dev") });
 
 export const SEVER_PORT = process.env.SEVER_PORT;
 export const DB_URI = process.env.DB_URI;
+export const SECRET_KEY = process.env.SECRET_KEY;
